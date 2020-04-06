e8cd1e0817d200809e9e4701c01dd145dfa63afa
Aaron Gustafson <aaron@easy-designs.net>
2020-04-06 14:27:10 -0700

    Tweaking the CMS setup & neighborhoods

--- src/filters/open-batch-filter.js
+++ src/filters/open-batch-filter.js
@@ -1,6 +1,5 @@
 module.exports = function openBatchFilter(array) {
   return array.filter(batch => {
-    console.log(batch.name);  
     if ( batch.open )
     {
       return Date.now() < Date.parse( batch.end );
