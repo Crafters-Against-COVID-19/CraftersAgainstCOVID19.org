040265fba2c2b6f42f3904da4cd7b9205b1c45a0
Aaron Gustafson <aaron@easy-designs.net>
2020-05-07 11:14:34 -0700

    Setting open batch filter to cut off at noon

--- src/filters/open-batch-filter.js
+++ src/filters/open-batch-filter.js
@@ -1,5 +1,5 @@
 module.exports = function openBatchFilter(array) {
   return array.filter(batch => {
-    return Date.now() < Date.parse( batch.end );
+    return Date.now() < Date.parse( batch.end + "T12:00:00" );
   });  
 };
