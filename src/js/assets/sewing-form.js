b1b591c47fdd4ae8f2b81867645bc60bbbbdbd31
Aaron Gustafson <aaron@easy-designs.net>
2020-05-30 22:16:13 -0700

    Reverted

--- src/js/assets/sewing-form.js
+++ src/js/assets/sewing-form.js
@@ -15,7 +15,7 @@
     {
       batch = open_batches[count];
       cutoff = new Date( batch.cutoff + "T12:00:00" );
-      cutoff = new Date( batch.cutoff + " 12:00:00 GMT-07:00" );
+      // cutoff = new Date( batch.cutoff + " 12:00:00 GMT-07:00" );
       if ( cutoff > now )
       {
         updated_batches.unshift( batch );
@@ -53,7 +53,7 @@
       if ( batch.pattern_name == pattern )
       {
         end_date = new Date( batch.end + "T12:00:00" );
-        end_date = new Date( batch.end + " 12:00:00 GMT-0700" );
+        // end_date = new Date( batch.end + " 12:00:00 GMT-0700" );
         day = days[end_date.getDay()];
         date = ( end_date.getMonth() + 1 ) + "/" + end_date.getDate();
         $batch_name.value = batch.name;
