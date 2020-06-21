8a4548a999ffa9313b1b4feb6c0e7eedeacfd5f5
Aaron Gustafson <aaron@easy-designs.net>
2020-06-21 12:12:49 -0700

    removing the request thank you from the Service Worker

--- src/_includes/partials/global/service-worker.js
+++ src/_includes/partials/global/service-worker.js
@@ -8,6 +8,7 @@ const EXCLUDED_URLS = [
   'admin',
   '.netlify',
   '/volunteer/thanks',
+  '/request/thanks',
   'https://identity.netlify.com/v1/netlify-identity-widget.js',
   'https://unpkg.com/netlify-cms@^2.9.3/dist/netlify-cms.js'
 ];
