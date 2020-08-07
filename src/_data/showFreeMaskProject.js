dd6913c3d5868ee4a7c5abb2c8746d332fd93617
Aaron Gustafson <aaron@easy-designs.net>
2020-08-06 21:13:18 -0700

    Real date

--- src/_data/showFreeMaskProject.js
+++ src/_data/showFreeMaskProject.js
@@ -1,6 +1,6 @@
 module.exports = function(){
   var now = Date.now(),
-      start = new Date( Date.UTC( 2020, 7, 6, 06, 59, 59 ) ),
+      start = new Date( Date.UTC( 2020, 7, 7, 06, 59, 59 ) ),
       end =  new Date( Date.UTC( 2020, 7, 15, 06, 59, 59 ) );
   return ( now > start ) && ( now < end );
 };
